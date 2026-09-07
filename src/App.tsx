e3643173b4ea1a9da0f518ac9ebbb0b3ec33e227
Slypot <sylvain.pottier@ynov.com>
2026-09-07 15:08:11 +0200

    Agent des agents et système de points

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import StudentCard from "./components/User";
+import AgentValorant from "./components/User";
+import "./App.css";
+
+interface Agent {
+  id: number;
+  firstName: string;
+  image: string;
+}
 
 function App() {
-  const agents = [
-    { id: 1, firstName: "David", lastName: "Gomez", age: 20 },
-    { id: 2, firstName: "Gabriel", lastName: "Piras", age: 18 }
+  const agents: Agent[] = [
+    { id: 1, firstName: "Raze", image: "https://i.imgur.com/hiuGumq.png" },
+    { id: 2, firstName: "Neon", image: "https://i.imgur.com/at4Vde4.png" }, 
+    { id: 3, firstName: "Jett", image: "https://i.imgur.com/9ol5pap.png" },
+    { id: 4, firstName: "Omen", image: "https://i.imgur.com/FUKpAru.png" }    
   ];
 
   return (
     <main>
       <h1>Défi Final</h1>
-      {agents.map((agent) => (
-        <AgentValorant
-          key={agent.id}
-          firstName={agent.firstName}
-          lastName={agent.lastName}
-          age={agent.age}
-        />
-      ))}
+      <h2>Agents Valorant</h2>
+      <h3>Choisis l'agent que tu préfères jouer:</h3>
+      <div className="agents-container">
+        {agents.map((agent) => (
+          <AgentValorant
+            key={agent.id}
+            firstName={agent.firstName}
+            image={agent.image}
+          />
+        ))}
+      </div>
     </main>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-function AgentValorant({ firstName, lastName, age }: { firstName: string; lastName: string; age: number }) {
+function AgentValorant({ firstName, image }: { firstName: string; image: string }) {
   const [points, setPoints] = useState(0);
 
   return (
     <section>
-      <h2>{firstName} {lastName}</h2>
-      <p>Age: {age}</p>
-      <p>Points: {points}</p>
+      <h2>{firstName}</h2>
+      <img src={image} alt={firstName} />
+      <p>👍: {points}</p>
       <button onClick={() => setPoints(points + 1)}>
         Ajouter un point
       </button>
